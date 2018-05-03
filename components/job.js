ec458d7e29c70864441323d18c4a998839c69747
Paul Summers <paulsumm@microsoft.com>
2018-05-03 11:07:49 -0400

    HTTPS JSON

--- components/job.js
+++ components/job.js
@@ -1,7 +1,7 @@
     const EventEmitter = require('events');
-    const http = require('http');
+    const https = require('https');
 
-    const httpUrl = 'http://nodeprogram.com'
+    const httpUrl = 'https://gist.githubusercontent.com/azat-co/a3b93807d89fd5f98ba7829f0557e266/raw/43adc16c256ec52264c2d0bc0251369faf02a3e2/gistfile1.txt'
 
     class Job extends EventEmitter {
         constructor(ops){
@@ -12,15 +12,23 @@
         }
 
         process(){
-            http.get(httpUrl, (response) => {
+            https.get(httpUrl, (response) => {
                 let data = ""
                 response.on('data', (dataChunk) => {
                     data += dataChunk.toString('utf8');
                 });
                 response.on('end', () => {
-                    console.log("Request complete");
-                    this.emit('done', {completedOn: new Date(), htmlData: data});
+                    try {
+                        const jsonData = JSON.parse(data)
+                        this.emit('done', {completedOn: new Date(), responseData: jsonData});
+                    }
+                    catch(e){
+                        this.emit('error', {errorData: e})
+                    }
+                    
                 });
+            }).on('error', (error) => {
+                this.emit('error', {errorData: error })
             })
         }
 
--- components/weekly.js
+++ components/weekly.js
@@ -2,8 +2,12 @@ var Job = require('./job.js');
 var job = new Job();
 
 job.on('done', function(details){
-    console.log(details.htmlData);
+    console.log(details.responseData);
     console.log('Weekly job completed on: ', details.completedOn)
+})
+
+job.on('error', function(error) {
+    console.log('Error occurred: ' + error)
 })
 
 job.emit('start');
