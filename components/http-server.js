8f52d5c6a84eae2d3ab6650130c879e91a277ec4
Paul Summers <paulsumm@microsoft.com>
2018-05-03 12:04:59 -0400

    Log request elements

--- components/http-server.js
+++ components/http-server.js
@@ -2,6 +2,9 @@ const http = require('http')
 const port = 3000
 
 http.createServer((req, res) => {
+    console.log(req.headers)
+    console.log(req.method)
+    console.log(req.url)
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('Hello World\nHello Paul')
 }).listen(port)
