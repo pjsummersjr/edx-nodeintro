ff9239896eaafdc67348375a9cfe3fb41da7f3d1
Paul Summers <paulsumm@microsoft.com>
2018-05-08 07:10:44 -0400

    Basic json output - probably not totally corrrect

--- components/csvtojson/labapp.js
+++ components/csvtojson/labapp.js
@@ -1,8 +1,14 @@
 const CsvJsonConverter = require('./csvtojson.js')
+const fs = require('fs')
 
 const csvFileUrl = 'https://prod-edxapp.edx-cdn.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+1T2018+type@asset+block/customer-data.csv'
+const csvFileLocation = "C:/dockerprojects/edx/edx-nodeintro/data/data.csv"
 const jsonFilePath = 'csvasjson.json'
 
 let converter = new CsvJsonConverter()
 
-converter.csvtojson(csvFileUrl, jsonFilePath)
+jsonData = fs.readFileSync(csvFileLocation, {encoding:'utf8'})
+console.log(jsonData)
+converter.csvdatatojson(jsonData, jsonFilePath)
+
+//converter.csvtojson(csvFileUrl, jsonFilePath)
