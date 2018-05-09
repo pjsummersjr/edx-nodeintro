be2f516246d4866d2ebd866140fe4ca97f448357
Paul Summers <paulsumm@microsoft.com>
2018-05-09 16:19:52 -0400

    Express config abstraction - dependency injection

--- components/express-app/express-server.js
+++ components/express-app/express-server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const ExpressConfig = require('./config/express-config.js')
 const app = express()
+const expressConfig = new ExpressConfig()
+
+app.set('port', expressConfig.PORT)
+app.set('views', expressConfig.VIEW_ENGINE)
+app.set('view engine', expressConfig.VIEW_ENGINE)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Jungle!')
 })
 
-app.listen(3000)
+app.listen()
