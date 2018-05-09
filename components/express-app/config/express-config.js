f8d2a8bfe3dcec1c04d3c01d6ec7f57f7d93940f
Paul Summers <paulsumm@microsoft.com>
2018-05-09 16:19:16 -0400

    Express config abstraction - dependency injection

---
+++ components/express-app/config/express-config.js
@@ -0,0 +1,9 @@
+import { builtinModules } from "module";
+
+class ExpressConfig {
+    const PORT = 3000;
+    const VIEWS_DIR = 'views'
+    const VIEW_ENGINE = 'jade'
+}
+
+module.exports = ExpressConfig
